12423bbb8be8c96ced3a8c2b7675eb549e38ec7d
mak-p <mak-p@DESKTOP-PG1IP70>
2022-11-15 10:42:30 +0300

    Hardcore day 1

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 #define delimiter "\n--------------------------------------------\n"
@@ -8,8 +10,8 @@ using namespace std;
 
 class Human
 {
-	string last_name;
-	string first_name;
+	std::string last_name;
+	std::string first_name;
 	unsigned int age;
 public:
 	const string& get_last_name()const
@@ -39,7 +41,23 @@ public:
 	// Methods
 	virtual ostream& print(ostream& os)const
 	{
-		return os << last_name << " " << first_name << " " << age << " лет\n";
+		return os << last_name << " " << first_name << " " << age << " лет";
+	}
+	virtual std::ofstream& print(std::ofstream& ofs)const
+	{
+		//ofs << last_name << " " << first_name << " " << age;
+		ofs.width(25);
+		ofs << std::left;
+		ofs << last_name + " " + first_name; 
+		ofs.width(5);
+		ofs << std::right;
+		ofs << age;
+		return ofs;
+	}
+	virtual std::ifstream& scan(std::ifstream& ifs)
+	{
+		ifs >> last_name >> first_name >> age;
+		return ifs;
 	}
 };
 
@@ -47,6 +65,11 @@ ostream& operator<<(ostream& os, const Human& obj)
 {
 	return obj.print(os);
 }
+std::ofstream& operator<<(std::ofstream& ofs, const Human& obj)
+{
+	return obj.print(ofs);
+}
+
 #define STUDENT_TAKE_PARAMETERS const string& specialty, const string& group, double rating, double attendence
 #define STUDENT_GIVE_PARAMETERS specialty,group,rating,attendence
 
@@ -108,7 +131,33 @@ public:
 	// Methods
 	ostream& print(ostream& os)const override
 	{
-		return Human::print(os) << specialty << " " << group << " " << rating << " " << attendence << endl;
+		return Human::print(os) << "," << specialty << " " << group << " " << rating << " " << attendence;
+	}
+	std::ofstream& print(std::ofstream& ofs)const override
+	{
+		Human::print(ofs) << " ";
+		ofs.width(25);
+		ofs << std::left;
+		ofs << specialty;
+		ofs.width(10);
+		ofs << group;
+		ofs.width(8);
+		ofs << std::right;
+		ofs << rating;
+		ofs.width(8);
+		ofs << attendence;
+		return ofs;
+	}
+	std::ifstream& scan(std::ifstream& ifs)override
+	{
+		const int SIZE = 25;
+		char buffer[SIZE]{};
+		ifs.read(buffer, SIZE);
+		specialty = buffer;
+		ifs >> group;
+		ifs >> rating;
+		ifs >> attendence;
+		return ifs;
 	}
 };
 
@@ -155,8 +204,26 @@ public:
 
 	ostream& print(ostream& os)const override
 	{
-		Human::print(os);
-		return os << specialty << " " << experience << endl;
+		return Human::print(os)<< "," << specialty << " " << experience;
+	}
+	std::ofstream& print(ofstream& ofs)const override
+	{
+		Human::print(ofs) << " ";
+		ofs.width(25);
+		ofs << std::left;
+		ofs << specialty;
+		ofs.width(5);
+		ofs << std::right;
+		ofs << experience;
+		return ofs;
+	}
+	std::ifstream& scan(std::ifstream& ifs)override
+	{
+		const int SIZE = 25;
+		char buffer[SIZE]{};
+		ifs.read(buffer, SIZE);
+		ifs >> experience;
+		return ifs;
 	}
 };
 
@@ -191,10 +258,72 @@ public:
 	// Methods
 	ostream& print(ostream& os)const override
 	{
-		Student::print(os);
-		return os << "Тема дипломной работы: " << topic << endl;
+		return Student::print(os) << ", Тема дипломной работы: " << topic;
+	}
+	std::ofstream& print(ofstream& ofs)const override
+	{
+		Student::print(ofs) << " " << topic;
+		return ofs;
+	}
+	std::ifstream& scan(std::ifstream& ifs)override
+	{
+		std::getline(ifs, topuic);
+		return ifs;
 	}
 };
+
+void print(Human* group[], const int n)
+{
+	// Specialisation - Уточнение (DownCast - преобразование сверху вниз) 
+	for (int i = 0; i < n; i++)
+	{
+		//group[i]->print();
+		cout << *group[i] << endl;
+		cout << delimiter << endl;
+	}
+}
+void save(Human* group[],const int n, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (int i = 0; i < n; i++)
+	{
+		//group[i]->print();
+		fout.width(15);
+		fout << std::left;
+		fout << typeid(*group[i]).name() << ":\t";
+		fout << *group[i] << endl;
+	}
+	fout.close();
+	std::string command = "notepad " + filename;
+	system(command.c_str());
+}
+Human** load(const std::string& filename,int& n)
+{
+	std::ifstream fin(filename);
+	// 1)Вычисляем размер массива
+	if (fin.is_open())
+	{
+		std::string buffer;
+		for(n=0;!fin.eof();n++)
+		{
+			std::getline(fin, buffer);
+		}
+		n--;
+	}
+	//2) Выделяем память под массив
+	Human** group = new Human * [n] {};
+	
+	//3) Возвращаемся в начало файла,для того чтобы прочитать строки
+	// и загрузить их в объекты
+	fin.clear();
+	fin.seekg(0);
+	//cout << fin.tellg() << endl;
+
+	//4) Загружаем объекты из файла
+
+
+	return group;
+}
 
 //#define INHERITANCE 
 
@@ -217,7 +346,7 @@ void main()
 	grad.print();
 #endif // INHERITANCE
 
-	Human* group[] =
+	/*Human* group[] =
 	{
 		new Student("Pinkman", "Jessie", 25, "Chemistry", "ww_220", 90, 95),
 		new Teacher("White", "Walter", 50, "Chemistry", 20),
@@ -225,16 +354,23 @@ void main()
 		new Student("Vercetti","Tomas",30,"Criminalistica","Vice",98,99),
 		new Teacher("Diaz","Ricardo",50,"Weapons distribution",15),
 		new Teacher("Einstein","Albert",143,"Astronomy",120)
-	};
-	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	};*/
+	// Specialisation - Уточнение (DownCast - преобразование сверху вниз)
+	/*for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
 	{
-		//group[i]->print();
 		cout << *group[i] << endl;
 		cout << delimiter << endl;
-	}
+	}*/
+	//print(group, sizeof(group) / sizeof(group[0]));
+	//save(group, sizeof(group) / sizeof(group[0]), "group.txt");
+	
+	int n = 0;
+	Human** group = load("group.txt", n);
+	print(group, n);
+
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
 		delete group[i];
 	}
-
+	
 }
--- Files/main.cpp
+++ Files/main.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 
 //#define WRITE_TO_FILE
-#define READ_TO_FILE
+//#define READ_TO_FILE
 
 void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef WRITE_TO_FILE
-	std::ofstream fout;            // 1) Создаем поток
-	fout.open("File.txt", std::ios::app);         // 2) Открываем поток
+	std::ofstream fout;                  // 1) Создаем поток
+	fout.open("File.txt", std::ios::app);// 2) Открываем поток
 	//std::ios::app - append,не переписывает,а дописывает в конец файла
 
-	fout << "Hello Files" << endl; // 3) Пишем в поток
-	fout.close();                  // 4) Закрываем поток
+	fout << "Hello Files" << endl;       // 3) Пишем в поток
+	fout.close();                        // 4) Закрываем поток
 
-	system("notepad File.txt"); // Функция system() запускаем любую программу к которой есть PATH(путь)  
+	system("notepad File.txt");          // Функция system() запускаем любую программу к которой есть PATH(путь) В Windows 
 #endif // WRITE_TO_FILE
 
+#ifdef READ_TO_FILE
 	ifstream fin("File.txt");
 	if (fin.is_open())
 	{
@@ -37,4 +38,15 @@ void main()
 	{
 		std::cerr << "Error: file not found" << endl;
 	}
+#endif // READ_TO_FILE
+
+	std::ofstream fout;
+	fout.open("File.txt", std::ios::app);
+	fout<<"new Student Pinkman Jessie 25 Chemistry ww_220 90 95"
+		new Teacher("White", "Walter", 50, "Chemistry", 20),
+		new Undergrad("Makhmudov", "Punkhan", 35, "Software developer", "PV_224", 95, 80, "OOP Polymorphism"),
+		new Student("Vercetti", "Tomas", 30, "Criminalistica", "Vice", 98, 99),
+		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 15),
+		new Teacher("Einstein", "Albert", 143, "Astronomy", 120)"
+
 }
